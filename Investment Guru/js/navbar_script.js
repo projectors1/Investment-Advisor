9acab78b6091be052f6167b5775f9dee09e80f05
i1-D <ivanmiap@live.com>
2017-08-14 23:05:51 +0530

    Updated different sections to about us page

--- Investment Guru/js/navbar_script.js
+++ Investment Guru/js/navbar_script.js
@@ -4,7 +4,7 @@ $(function(){
         if($(window).scrollTop() <= 20){
        		navbar.css('box-shadow', 'none');
         } else {
-          navbar.css('box-shadow', '0px 5px 20px rgba(0, 0, 0, 0.4)'); 
+          navbar.css('box-shadow', '0px 2px 5px rgba(0, 0, 0, 0.26)'); 
         }
     });  
 })
