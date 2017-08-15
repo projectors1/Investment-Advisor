b3d88636ba743272ed88bcacea9631848a523b96
i1-D <ivanmiap@live.com>
2017-08-15 23:47:55 +0530

    Added footer

---
+++ Investment Guru/js/footer_script.js
@@ -0,0 +1,4 @@
+$("a[href='#top']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+});
--- Investment Guru/js/navbar_script.js
+++ Investment Guru/js/navbar_script.js
@@ -1,3 +1,4 @@
+
 $(function(){
     var navbar = $('.navbar');
     $(window).scroll(function(){
