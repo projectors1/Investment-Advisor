39aefb264b34ecda3c477cb40f4a8fdb19654d59
i1-D <ivanmiap@live.com>
2017-09-16 13:27:37 +0530

    Added scroll animation of index page

--- Investment Guru/js/footer_script.js
+++ Investment Guru/js/footer_script.js
@@ -2,3 +2,8 @@ $("a[href='#top']").click(function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
 });
+
+$("#btn_started").click(function() {
+    $("html, body").animate({ scrollTop: $('#form-section').offset().top }, "slow");
+    return false;
+});
