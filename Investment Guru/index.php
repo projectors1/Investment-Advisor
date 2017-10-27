c373be7853ae61972a3857a7aab65ad3a96ab891
i1-D <ivanmiap@live.com>
2017-10-27 11:18:00 +0530

    Removed dashboard

--- Investment Guru/index.php
+++ Investment Guru/index.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Investment Guru</title>
+    <title>Goal Tracker</title>
     <link rel="icon" href="img/logo.png">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- Investment Guru/sidebar.php
+++ Investment Guru/sidebar.php
@@ -8,9 +8,6 @@
     </div>
     <ul class="nav nav-list">
         <li class="header">Navigation Menu</li>
-         <li <?php if (basename($_SERVER['PHP_SELF']) == 'dashboard.php') echo 'class="active"' ?>>
-            <a href="dashboard.php"><i class="fa fa-pie-chart fa-fw" aria-hidden="true"></i>Dashboard</a>
-        </li>
         <li <?php if (basename($_SERVER['PHP_SELF']) == 'profile.php') echo 'class="active"' ?>>
             <a href="profile.php"><i class="fa fa-address-card fa-fw" aria-hidden="true"></i>Profile</a>
         </li>
